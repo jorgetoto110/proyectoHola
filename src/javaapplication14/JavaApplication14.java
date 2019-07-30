95c385952c3b42744e83b70ad36b91d25a1513c8
jorgetoto110 <jorge_toto110@hotmail.com>
2019-07-30 15:44:23 -0300

    cambie el hola, por hola desde gitHub

--- src/javaapplication14/JavaApplication14.java
+++ src/javaapplication14/JavaApplication14.java
@@ -15,7 +15,7 @@ public class JavaApplication14 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola desde netbeans");
+        System.out.println("hola desde gitHub");
        
     }
     
